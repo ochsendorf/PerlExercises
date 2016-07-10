e727d1129a43d294c3c857bfce92fe8c019eb8ea
ochsendorf <thiago.ochsendorf@gmail.com>
2016-07-10 19:06:41 -0300

    reading properties

--- HandleFiles.pl
+++ HandleFiles.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+use Config::Simple
 
 # open file
 # open FH, "<project.properties";
@@ -15,10 +16,16 @@ print FH "email = email_email\n";
 print "done. closing file\n \n";
 close(FH);
 
-open FH, "<project.properties";
-while ( $line = <FH> ) {
-  print $line;
+# reading the entire file content + print
+#open FH, "<project.properties";
+#while ( $line = <FH> ) {
+#  print $line;
 # last if $. == 2;
-}
+#}
+#close(FH);
 
-close(FH);
+# using Config::Properties to get properties values
+$cfg = new Config::Simple("project.properties");
+$name = $cfg->param("name");
+print "the property name has value = " . $name . "\n";
+
